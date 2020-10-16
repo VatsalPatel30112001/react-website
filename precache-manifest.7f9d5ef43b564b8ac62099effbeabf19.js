4be1010d301b5b681bdf4272a5ddf40fbeddd9eb
vatsal <vatsal30112001@gmail.com>
2020-10-16 11:14:40 +0530

    Updates

---
+++ precache-manifest.7f9d5ef43b564b8ac62099effbeabf19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69c112b8ecec0663994463d82fb20e5",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "f400545f4832a5d6cac8",
+    "url": "/react-website/static/css/main.1fd9f574.chunk.css"
+  },
+  {
+    "revision": "0745d241d08caf05a31a",
+    "url": "/react-website/static/js/2.ad067e2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website/static/js/2.ad067e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f400545f4832a5d6cac8",
+    "url": "/react-website/static/js/main.d4e890c5.chunk.js"
+  },
+  {
+    "revision": "409e9de06b6bd7048c0b",
+    "url": "/react-website/static/js/runtime-main.d50b4001.js"
+  },
+  {
+    "revision": "12f0994dc80301218fd43c028fc4adab",
+    "url": "/react-website/static/media/cartoon.12f0994d.jpg"
+  },
+  {
+    "revision": "ba3c59fbba13f6ad9356e08ec74d4aa1",
+    "url": "/react-website/static/media/lion.ba3c59fb.jpg"
+  }
+]);
